eb20fe54d675fa9d6be2e988f2aa139412d8d777
Maruf Hasan Shihab <devmarufhasan@gmail.com>
2025-03-07 03:56:00 +0600

    added latest blog section and email subscribe section to home page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import BlogList from "@/components/BlogList";
 import Header from "@/components/Header";
 
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <>
     <Header/>
+    <BlogList/>
     </>
   );
 }
