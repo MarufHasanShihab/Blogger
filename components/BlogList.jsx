47fe7fa3fca1ead7e5ae0136275558129d9f81ee
Maruf Hasan Shihab <devmarufhasan@gmail.com>
2025-03-08 12:38:29 +0600

    added blog filter category feature

--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -1,19 +1,58 @@
-import { blog_data } from "@/Assets/assets"
-import BlogItems from "./BlogItems"
+'use client'
+import { blog_data } from '@/Assets/assets'
+import BlogItems from './BlogItems'
+import { useState } from 'react'
 
 const BlogList = () => {
+  const [menu, setMenu] = useState('All')
   return (
     <div>
       <div className="flex justify-center gap-6 my-10">
-        <button className="bg-black text-white py-1 px-4 rounded-sm">All</button>
-        <button>Technology</button>
-        <button>Startup</button>
-        <button>Lifestyle</button>
+        <button
+          onClick={() => setMenu('All')}
+          style={{cursor:"pointer"}}
+          className={
+            menu === 'All' ? 'bg-black text-white py-1 px-4 rounded-sm' : ''
+          }
+        >
+          All
+        </button>
+        <button
+          onClick={() => setMenu('Technology')}
+          style={{cursor:"pointer"}}
+          className={
+            menu === 'Technology'
+              ? 'bg-black text-white py-1 px-4 rounded-sm'
+              : ''
+          }
+        >
+          Technology
+        </button>
+        <button
+          onClick={() => setMenu('Startup')}
+          style={{cursor:"pointer"}}
+          className={
+            menu === 'Startup' ? 'bg-black text-white py-1 px-4 rounded-sm' : ''
+          }
+        >
+          Startup
+        </button>
+        <button
+          onClick={() => setMenu('Lifestyle')}
+          style={{cursor:"pointer"}}
+          className={
+            menu === 'Lifestyle'
+              ? 'bg-black text-white py-1 px-4 rounded-sm'
+              : ''
+          }
+        >
+          Lifestyle
+        </button>
       </div>
       <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
-        {
-          blog_data.map((blog)=><BlogItems key={blog.title} blog={blog}/>)
-        }
+        {blog_data?.filter(blog=>menu==="All"?true:blog.category===menu).map((blog) => (
+          <BlogItems key={blog.title} blog={blog} />
+        ))}
       </div>
     </div>
   )
